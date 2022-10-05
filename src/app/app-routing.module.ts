db26c6016b952f16e10d71afbaa09d6d1b5713b4
Tatiane Mossi <mossitatiane2@gmail.com>
2022-10-05 11:29:47 -0300

    Implementando login de usuários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'conta/registrar', pathMatch: 'full' },
+  { path: '', redirectTo: 'conta/autenticar', pathMatch: 'full' },
+  { path: 'conta/autenticar', component: LoginComponent },
   { path: 'conta/registrar', component: RegistroComponent },
   {
     path: 'dashboard',
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,11 +6,12 @@ import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LocalStorageService } from './services/local-storage.service';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [RegistroComponent],
+  declarations: [RegistroComponent, LoginComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
